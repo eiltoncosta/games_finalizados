4bb8a158236c4d6a016308dd5b89b558d5cef94d
José Eilton <132386956+eiltoncosta@users.noreply.github.com>
2025-01-26 19:49:23 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (!sessionStorage.getItem('alertShown') && (window.location.pathname === '/index.html' || window.location.pathname === '/')) {
+      setTimeout(function () {
+        alert('Este site atualiza com frequência!');
+        sessionStorage.setItem('alertShown', 'true');
+      }, 2000); // Espera 2 segundos
+    }
+  });
+  
+const btnAvancar = document.getElementById("btn-avancar")
+const cartoes = document.querySelectorAll(".cartao")
+let cartaoAtual = 0;
+
+
+btnAvancar.addEventListener("click", function (){
+
+if(cartaoAtual === cartoes.length -1) return;
+
+
+const cartaoSelecionado = document.querySelector(".selecionado");
+cartaoSelecionado.classList.remove("selecionado");
+
+
+cartaoAtual++;
+cartoes[cartaoAtual].classList.add("selecionado");
+
+});
+
+
+
+const btnVoltar = document.getElementById("btn-voltar");
+
+btnVoltar.addEventListener("click", function (){
+
+if(cartaoAtual === 0) return;
+
+
+const cartaoSelecionado = document.querySelector(".selecionado");
+cartaoSelecionado.classList.remove("selecionado");
+
+
+cartaoAtual--;
+cartoes[cartaoAtual].classList.add("selecionado");
+
+});
